0689c3bf39a3dfc447a0c9b66265677eea651673
Brendan Bank <brendan.bank@gmail.com>
2026-02-15 11:40:25 +0100

    v1.3: GitHub Pages pkg repo, firmware registration, documentation
    Host pkg repo and Sphinx documentation via GitHub Pages so the OPNsense
    UI info button can display plugin details. Register/deregister plugin
    with firmware on install/remove. Add pre-install hook to reverse core
    patch before upgrade. Update maintainer email and WWW.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,7 @@
 project = 'KEA DDNS'
+author = 'Brendan Bank'
+copyright = 'Brendan Bank. Licensed under the BSD 2-Clause License'
 extensions = ['sphinx_tabs.tabs']
 html_theme = 'sphinx_rtd_theme'
-exclude_patterns = ['README.md', 'Makefile', '*.py', '*.html', '*.css']
+html_copy_source = False
+exclude_patterns = ['README.md', 'Makefile', '*.py', '*.html', '*.css', '*.js', 'repo']
